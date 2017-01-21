5019cc1dca4978866da8dd70b4bb6e7847de8833
Lori Dowers <ldowers@cfl.rr.com>
2017-01-21 18:13:55 -0500

    Added JAWSDB

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,17 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-    port: 3306,
-    host: "localhost",
-    user: "root",
-    password: "erik1990",
-    database: "cat_db"
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "erik1990",
+        database: "cat_db"
+    });
+}
 
 // Make connection.
 connection.connect(function(err) {
